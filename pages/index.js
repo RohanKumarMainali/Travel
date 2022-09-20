8dc5d9af7b4730fe83c37cc67cba3b5d25a2e8ee
rohan mainali <rohanmainali39@gmail.com>
2022-09-20 01:20:58 -0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,9 @@ export default function Home() {
       </Head>
       <Navbar></Navbar>
       <IndexHome/>
-      <Collection />
+    
       <PopularPackages/>
+      <Collection />
     </div>
   )
 }
