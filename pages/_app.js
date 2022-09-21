f80509151266d024da6182b262ca02c3e9f9ceb7
rohan mainali <rohanmainali39@gmail.com>
2022-09-20 23:17:54 -0700

    offer package completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import Script from 'next/script'
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
 function MyApp({ Component, pageProps }) {
   return (
     <Head>
