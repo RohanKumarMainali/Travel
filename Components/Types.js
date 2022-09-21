2ba515be2e508138ff1c62fd509b812bd2d44a08
Samishan11 <samishanthapa0@gmail.com>
2022-09-21 17:24:49 +0545

    fix

--- Components/Types.js
+++ Components/Types.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaHiking } from 'react-icons/fa';
 import Style from "../public/css/type.module.css"
 const Types = () => {
+
     return (
         <div className="container my-5">
             <div className="contentTop text-center">
@@ -10,17 +11,16 @@ const Types = () => {
                 <img src="/image/section-title-vector.svg" alt="" />
             </div>
             <div className="row">
-                <div className={`col-md-2 py-5 col-sm-6 col-12 ${Style.item}`}>
-                    <div className={`icons mx-auto d-flex justify-content-center align-items-center rounded-circle ${Style.icondiv}`}>
-                        <div className={Style.animi}>
-                            <FaHiking className={Style.icon} />
+                    <div className={`col-md-2 py-5 col-sm-6 col-12 ${Style.item}`}>
+                        <div className={`icons mx-auto d-flex justify-content-center align-items-center rounded-circle ${Style.icondiv}`}>
+                            <div className={Style.animi}>
+                                <FaHiking className={Style.icon} />
+                            </div>
+                        </div>
+                        <div className="text mt-3">
+                            <h4 className='text-center'>Adventure</h4>
                         </div>
                     </div>
-                    <div className="text mt-3">
-                        <h4 className='text-center'>Adventure</h4>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,8 @@
-import '../styles/globals.css'
-import Head from 'next/head'
-import Script from 'next/script'
+import '../styles/globals.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 function MyApp({ Component, pageProps }) {
   return (
-    <Head>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-      ></link>
       <Component {...pageProps} />
-      <Script>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossorigin="anonymous"
-        ></script>
-      </Script>
-    </Head>
   )
 }
 
