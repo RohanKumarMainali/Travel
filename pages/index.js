7273c2e8a75640b471850f069babc0da7f9f598b
rohan mainali <rohanmainali39@gmail.com>
2022-09-22 00:10:08 -0700

    refined ui

--- pages/index.js
+++ pages/index.js
@@ -30,15 +30,21 @@ export default function Home() {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossorigin="anonymous"
         ></link>
-      
       </Head>
-      <Script>
-          <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
-          ></script>
-        </Script>
+      <Script
+        id="my_script"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        async
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
+        <Script
+        id="my_script"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        defer
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
       <Navbar></Navbar>
       <MainHome />
       <Search />
@@ -57,7 +63,6 @@ export default function Home() {
 
       <footer>
         <Footer></Footer>
-        
       </footer>
     </>
   )
