3d37694d6a35f5d1d762f504e77cdff4b41b2bfd
Samishan11 <samishanthapa0@gmail.com>
2022-09-22 16:16:09 +0545

    detail

--- Components/Detail.js
+++ Components/Detail.js
@@ -5,26 +5,19 @@ const Detail = () => {
         <div className="container mt-5 py-5">
             <div className="row">
                 <div className="col-md-8 mb-5 ">
-                    {/* <div className="sideIcons">
-                        <i class="bi bi-twitter"></i>
-                        <i class="bi bi-chat-left-dots"></i>
-                        <i class="bi bi-whatsapp"></i>
-                        <i class="bi bi-facebook"></i>
-                        <i class="bi bi-messenger"></i>
-                    </div> */}
-                    <div className="col-md-12 border">
-                        <h2>Overview</h2>
-                        <hr />
+                    <div className="col-md-12 border mb-4 p-4">
+                        <h3 className='fw-bold'>Overview</h3>
+                        <hr className='px-4' />
                         <p>Embark on an adventure that is the epitome of high-altitude trips as you make your way to the
                             Everest Base Camp. The journey offers a lavish spread of the Himalayas decorated with the
                             rich Sherpa culture and rich biodiversity.
                             The Everest Base Camp Trek (EBC Trek) is at the</p>
                     </div>
-                    <div className="border">
-                        <h2>Your Itinerary</h2>
-                        <hr />
+                    <div className="border mb-4 p-4">
+                        <h3 className='fw-bolder'>Your Itinerary</h3>
+                        <hr className='px-4' />
                         <div className="detail">
-                            <h4>Day 01 – Arrive at Tribhuvan International Airport Kathmandu.  </h4>
+                            <h5 className='text-secondary fw-bold'>Day 01 – Arrive at Tribhuvan International Airport Kathmandu.  </h5>
                             <p>
                                 Trek adventure begins with your arrival in Kathmandu City, the capital of Nepal. Upon your arrival,
                                 we’ll meet you at the Arrivals gate at Tribhuvan International Airport.
@@ -39,8 +32,7 @@ const Detail = () => {
                             </p>
                         </div>
                         <div className="detail">
-                            <h4>Day 03 –Phakding to Namche Bazaar (3440 m / 11283 ft) Walking Distance – 10 to 12 km, Duration: 6 hours </h4>
-                            <img src="/image/Namche.jpg" alt="" />
+                            <h5 className='text-secondary fw-bold'>Day 03 –Phakding to Namche Bazaar (3440 m / 11283 ft) Walking Distance – 10 to 12 km, Duration: 6 hours </h5>
                             <p>
                                 Trek adventure begins with your arrival in Kathmandu City, the capital of Nepal. Upon your arrival,
                                 we’ll meet you at the Arrivals gate at Tribhuvan International Airport.
@@ -56,11 +48,11 @@ const Detail = () => {
                         </div>
 
                     </div>
-                    <div className="border">
-                        <h2>Additional Info</h2>
-                        <hr />
+                    <div className="border p-4">
+                        <h3 className='fw-bold'>Additional Info</h3>
+                        <hr className='px-4' />
                         <div className="detail">
-                            <h1>Everest Base Camp Difficulty    </h1>
+                            <h2 className='fw-bolder'>Everest Base Camp Difficulty    </h2>
                             <p>
                                 Travel experts of Nepal have graded the difficulty of the Everest Base Camp trek as “moderate”. This means you will need very good fitness for walking 6 to 7 hours each day for about a week. However, there’s no need for any mountain climbing skills and gears to complete this trek. Basically, any person with good physical and mental fitness can complete this trek with a few months of preparation. Two to three months before the trek, you need to engage in some cardio exercises such as running, jogging, swimming, and cycling. It really helps to boost your stamina before you head for the trek. It’s easier than the Everest Three Pass Trek.
 
@@ -69,32 +61,143 @@ const Detail = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{position:'relative'}}  className="col-md-4 ">
-                    <div  style={{top:'10%', display: "unset"}} className="packages position-sticky border">
-                        <div className="package">
+                <div style={{ position: 'relative' }} className="col-md-4 bg-light ">
+                    <div style={{ top: '10%', display: "unset" }} className="package px-4 position-sticky ">
+                        <div className="package px-4 mt-3 px-4">
                             <h4>Package Summary</h4>
-                            <hr />
+                            <hr className='px-4' />
                         </div>
-                        <div className="package">
-                            <div className="dt d-flex">
-                                <p>Package Includes:</p>
-                                <i class="bi mx-1 bi-bicycle"></i>
-                                <i class="bi mx-1 bi-bicycle"></i>
-                                <i class="bi mx-1 bi-bicycle"></i>
-                                <i class="bi mx-1 bi-bicycle"></i>
+                        <div className="package px-4">
+                            <div className="dt d-flex align-items-center">
+                                <p className='me-2'>package px-4 Includes:</p>
+                                <i style={{ color: '#90b956' }} class="bi h3 mx-1 bi-bicycle"></i>
+                                <i style={{ color: '#90b956' }} class="bi h3 mx-1 bi-bicycle"></i>
+                                <i style={{ color: '#90b956' }} class="bi h3 mx-1 bi-bicycle"></i>
+                                <i style={{ color: '#90b956' }} class="bi h3 mx-1 bi-bicycle"></i>
                             </div>
-                            <hr />
+                            <hr className='px-4' />
                         </div>
-                        <div className="package">
-                            <h4>Highlights of Everest Base Camp Trek</h4>
-                            <hr />
+                        <div className="package px-4">
+                            <h5>Highlights of Everest Base Camp Trek</h5>
+                            <hr className='px-4' />
                         </div>
-                        <div className="package">
+                        <div className="package px-4">
                             <div className="dt d-flex">
-                                <i class="bi bi-check-circle-fill"></i>
+                                <i style={{ color: '#90b956' }} class="bi me-2 h5 bi-check-circle-fill"></i>
                                 <p>Experience the Lukla flight to one of the most Adventures airports in the world, Tenzing-Hillary Airport </p>
                             </div>
+                            <hr className='px-4' />
+                        </div>
+                        <div className="package px-4">
+                            <div className="dt d-flex justify-content-between">
+                                <p className='h5'>Package Price</p>
+                                <p className='h5'>USD $1194</p>
+                            </div>
                             <hr />
+                        </div>
+                        <div className="package px-4">
+                            <p className='h5 fw-bolder'>Join Our Fixed Departures</p>
+                            <hr className='px-4' />
+                        </div>
+                        <div style={{ height: "25vh", overflowY: 'scroll' }} className="package px-4 scrollable">
+                            <div className="title d-flex">
+                                <p className='fw-bold me-4'>Departing</p>
+                                <p className='fw-bold'>Trip status</p>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+                            <hr className='px-4' />
+                            <div className="status d-flex justify-content-between">
+                                <div className="date">
+                                    <p>26 Sep,2022</p>
+                                </div>
+                                <div className="booking">
+                                    <p>Booking Open</p>
+                                </div>
+                                <div className="booking-btn">
+                                    <button className='btn btn-outline-main'>Book Now</button>
+                                </div>
+                            </div>
+
+                        </div>
+                        <hr className='px-4' />
+                        <div className="package px-4 mt-2">
+                            <a style={{ fontSize: '20px' }} className="text-primary" href="#">Customize Your Trip</a>
+                            <hr className='px-4' />
+                        </div>
+                        <div className="package px-4 rating mt-2 d-flex">
+                            <div className="iocns-star d-flex">
+                                <i class="bi text-warning me-1 bi-star-fill"></i>
+                                <i class="bi text-warning me-1 bi-star-fill"></i>
+                                <i class="bi text-warning me-1 bi-star-fill"></i>
+                                <i class="bi text-warning me-1 bi-star-fill"></i>
+                            </div>
+                            <div className="text">
+                                <p className='text-primary'> - Based on 25 Reviews</p>
+                            </div>
+                        </div>
+                        <hr className='px-4' />
+                        <div className="package text-primary px-4 album d-flex justify-content-center align-items-center ">
+                            <i class="bi h4 me-2 bi-images"></i>
+                            <a className='h6' href="#">Photo Tour (Gallery)</a>
                         </div>
                     </div>
                 </div>
