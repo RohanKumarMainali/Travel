eddf4a18bf4e046f417c2ecbfe8c481278f96b20
Samishan11 <samishanthapa0@gmail.com>
2022-09-22 14:25:49 +0545

    detail

--- Components/Detail.js
+++ Components/Detail.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Detail = () => {
     return (
-        <div className="container">
+        <div className="container mt-5 py-5">
             <div className="row">
-                <div className="col-md-6">
-                    <div className="col-md-12">
+                <div className="col-md-8 mb-5 ">
+                    {/* <div className="sideIcons">
+                        <i class="bi bi-twitter"></i>
+                        <i class="bi bi-chat-left-dots"></i>
+                        <i class="bi bi-whatsapp"></i>
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-messenger"></i>
+                    </div> */}
+                    <div className="col-md-12 border">
                         <h2>Overview</h2>
                         <hr />
                         <p>Embark on an adventure that is the epitome of high-altitude trips as you make your way to the
@@ -13,8 +20,63 @@ const Detail = () => {
                             rich Sherpa culture and rich biodiversity.
                             The Everest Base Camp Trek (EBC Trek) is at the</p>
                     </div>
+                    <div className="border">
+                        <h2>Your Itinerary</h2>
+                        <hr />
+                        <div className="detail">
+                            <h4>Day 01 – Arrive at Tribhuvan International Airport Kathmandu.  </h4>
+                            <p>
+                                Trek adventure begins with your arrival in Kathmandu City, the capital of Nepal. Upon your arrival,
+                                we’ll meet you at the Arrivals gate at Tribhuvan International Airport.
+                                A representative from  My Everest Trip staff will meet you at the airport and
+                                help you with your luggage. Enjoy a brief tour of the bustling Kathmandu city
+                                as you make your way to the hotel. You will check into one of the finest Star-rated
+                                hotels located at the heart of the city. As there are no planned activities for this day,
+                                you can enjoy the refreshment and relax in your room. In the noon or evening, you can head out
+                                to explore the city. The neighboring town of Thamel is one of the most sought-after destinations
+                                in Kathmandu. Enjoy touring the place and visit the local shops highlighting pashmina shawls—overnight
+                                in Kathmandu
+                            </p>
+                        </div>
+                        <div className="detail">
+                            <h4>Day 03 –Phakding to Namche Bazaar (3440 m / 11283 ft) Walking Distance – 10 to 12 km, Duration: 6 hours </h4>
+                            <img src="/image/Namche.jpg" alt="" />
+                            <p>
+                                Trek adventure begins with your arrival in Kathmandu City, the capital of Nepal. Upon your arrival,
+                                we’ll meet you at the Arrivals gate at Tribhuvan International Airport.
+                                A representative from  My Everest Trip staff will meet you at the airport and
+                                help you with your luggage. Enjoy a brief tour of the bustling Kathmandu city
+                                as you make your way to the hotel. You will check into one of the finest Star-rated
+                                hotels located at the heart of the city. As there are no planned activities for this day,
+                                you can enjoy the refreshment and relax in your room. In the noon or evening, you can head out
+                                to explore the city. The neighboring town of Thamel is one of the most sought-after destinations
+                                in Kathmandu. Enjoy touring the place and visit the local shops highlighting pashmina shawls—overnight
+                                in Kathmandu
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="border">
+                        <h2>Additional Info</h2>
+                        <hr />
+                        <div className="detail">
+                            <h1>Everest Base Camp Difficulty    </h1>
+                            <p>
+                                Travel experts of Nepal have graded the difficulty of the Everest Base Camp trek as “moderate”. This means you will need very good fitness for walking 6 to 7 hours each day for about a week. However, there’s no need for any mountain climbing skills and gears to complete this trek. Basically, any person with good physical and mental fitness can complete this trek with a few months of preparation. Two to three months before the trek, you need to engage in some cardio exercises such as running, jogging, swimming, and cycling. It really helps to boost your stamina before you head for the trek. It’s easier than the Everest Three Pass Trek.
+
+                                The major Everest base camp trek difficulty is distance and altitude. Everest Base Camp distance is about 150 km and you will be covering this distance for 8 days. This means you will be walking 15 to 20 km each day. This makes 6 to 7 hours of the daily walk which surely demands good physical health. Along with distance, the altitude of Everest base camp 5300 M, and that of Kala Pathar is also a big challenge during the trek. Once you cross Namche Bazaar, you start to feel the pressure of low oxygen level which makes your climb harder. Covering 500 to 1000m each day and including a rest day in Everest Base Camp Trek Itinerary helps you a lot. However, this doesn’t mean you need oxygen for the Everest Base Camp trek.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-4"></div>
+                <div className="col-md-4 position-relative">
+                    <div className="packages position-fixed border">
+                        <div className="package">
+                            <h4>Package Summary</h4>
+                            <hr />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
