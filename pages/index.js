1632190c88710afac043e5f8d94e12620fde73c9
rohan mainali <rohanmainali39@gmail.com>
2022-09-21 23:59:33 -0700

    script

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,15 @@ export default function Home() {
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossorigin="anonymous"
         ></link>
-        <Script>
+      
+      </Head>
+      <Script>
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"
           ></script>
         </Script>
-      </Head>
       <Navbar></Navbar>
       <MainHome />
       <Search />
