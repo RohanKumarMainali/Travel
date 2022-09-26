adb43bbe3c8b2e593b6bc8d3bd44a544b4fdf683
rohan mainali <rohanmainali39@gmail.com>
2022-09-25 22:15:36 -0700

    page

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"
       ></Script>
-        <Script
+      <Script
         id="my_script"
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         defer
